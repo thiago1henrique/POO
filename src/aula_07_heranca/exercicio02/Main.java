8de73d73b32bc9e295d1ebb3a949e49511d72047
Thiago <1thiago1henrique@gmail.com>
2025-05-14 13:34:10 -0300

    teste lalalal

--- src/aula_07_heranca/exercicio02/Main.java
+++ src/aula_07_heranca/exercicio02/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
         System.out.println(v1.calcularPrecoCorrida(10, 20));
         System.out.println(v2.calcularPrecoCorrida(10, 20));
-        System.out.println(v3.calcularPrecoCorrida(10, 20));
+        System.out.println(v3.calcularPrecoCorrida(10, 30));
         System.out.println(v4.calcularPrecoCorrida(10));
     }
 }
